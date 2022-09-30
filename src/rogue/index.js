dd75e4d45cb3206df5239d1af02bb646597ca54f
Marissa Huysentruyt <marissa.huysentruyt@heysparkbox.com>
2022-09-30 16:58:13 -0400

    feat: add support for new character classes :sparkles:
    -Add a wizard and a rogue that extend the Character class.
    -Update individual character talk methods to return "YELLING"
    (.toUpperCase) if a wizard, and "whispering" (.toLowerCase) if a rogue.
    -Add support for wizard's attack method: he can cast a protection spell,
    he doubles his counter attacks, he takes 10 damage after the spell wears
    off
    -Add support for rogue's attack method: the first 4 times they're
    attacked, they counter attack at 5x, on the 5th attack, they take 5
    damage and only counter attack at 1x
    -Remove nested conditionals within respective attack methods in rogue and
    wizard files.
    
    FSA22V2-99

--- src/rogue/index.js
+++ src/rogue/index.js
@@ -1,11 +1,33 @@
 import Character from '../character';
 
-class Rogue {
+class Rogue extends Character {
+  constructor(name) {
+    super(name);
+    this.name = name.toLowerCase();
+    this.attackNumber = 5;
+  }
+
   talk() {
+    // the Character superclass has proper grammar and an exclamation point,
+    // but the rogue speaks in lowercase whispers and periods exclusively.
+    const originalSpeak = super.talk().toLowerCase();
+    return originalSpeak.replace('!', '.');
   }
 
   attack() {
+  // we need to count how many times sigmund has been attacked
+  // they can be attacked 4 times, during which they attack at 5X
+    if (this.attackNumber > 1) {
+      this.attackNumber -= 1;
+      return 5;
+    }
+    // on the 5th time, they attack at 1X and take 5 damage points
+    // we also reset this.attackNumber to 5 to start the whole loop again
+    this.attackNumber = 5;
+    this.health -= 5;
+    return 1;
   }
 }
 
+// const sigmund = new Rogue('Sigmund');
 export default Rogue;
--- src/wizard/index.js
+++ src/wizard/index.js
@@ -1,11 +1,39 @@
 import Character from '../character';
 
-class Wizard {
+class Wizard extends Character {
+  constructor(name, health) {
+    super(name, health);
+    this.name = name.toUpperCase();
+    this.spellActive = 0;
+    this.spellTriggered = false;
+  }
+
   talk() {
+    return super.talk().toUpperCase();
   }
 
   attack() {
+  // we need to count how many times merlin has been attacked.
+  // after being attacked once, he casts a spell to prevent the next 3 attacks
+    if (this.spellTriggered === false) {
+      this.spellTriggered = true;
+      this.spellActive = 3;
+      return 2;
+    }
+    // for those next 3 turns, take no damage, AND attacks at 2x
+    if (this.spellTriggered === true && this.spellActive > 1) {
+      // as long as spellActive is >0, return 2
+      // if (this.spellActive > 0) {
+      this.spellActive -= 1; // turn 2, turn 1, turn 0
+      return 2;
+    }
+    // after the spell fades, merlin takes 10 damage on the next time he's attacked,
+    this.health -= 10;
+    return 1;
+    // his own attacks go back to default attack value
   }
 }
+// }
 
+// const merlin = new Wizard('Merlin');
 export default Wizard;
